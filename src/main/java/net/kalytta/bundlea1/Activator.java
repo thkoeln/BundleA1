253ced4946584c45b4380f85d302d7bf1d8033ec
Endllesscrafter <endlesscrafter@gmail.com>
2018-01-17 20:22:02 +0100

    NummernVerwaltung und WebSeiten Implementierungen herangeholt um sie in der Buchverwaltung zu nutzen

--- src/main/java/net/kalytta/bundlea1/Activator.java
+++ src/main/java/net/kalytta/bundlea1/Activator.java
@@ -1,9 +1,12 @@
 package net.kalytta.bundlea1;
 
+import de.thkoeln.eks.osgi.zentralverwaltung.NummernVerwaltung;
+import de.thkoeln.eks.osgi.zentralverwaltung.WebSeiten;
 import java.util.Hashtable;
 import net.kalytta.bundlea1.buchver.BuchVerwaltungImpl;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
@@ -11,10 +14,27 @@ public class Activator implements BundleActivator {
     private ServiceRegistration buchverwaltungservice;
     
     public void start(BundleContext context) throws Exception {
-        // TODO add activation code here
-        Hashtable<String,String> properties = new Hashtable<String, String>();
         
-        buchverwaltungservice = context.registerService(BuchVerwaltungImpl.class.getName(), new BuchVerwaltungImpl(), properties);
+        
+        //Nummernverwaltung-Implementierung heranholen
+        ServiceReference[] refs = context.getServiceReferences("de.thkoeln.eks.osgi.zentralverwaltung.Nummernverwaltung", "(version=*)");
+        NummernVerwaltung nummernverwaltung = (NummernVerwaltung) context.getService(refs[0]);
+        
+        
+        //WebSeiten-Implementierung heranholen
+        ServiceReference[] refs2 = context.getServiceReferences("de.thkoeln.eks.osgi.zentralverwaltung.WebSeiten", "(version=*)");
+        WebSeiten webseiten = (WebSeiten) context.getService(refs[0]);
+        
+        //Eigene Service definieren und registrieren
+        Hashtable<String,String> properties = new Hashtable<String, String>();
+        properties.put("version", "1.0");
+        
+        BuchVerwaltungImpl bw = new BuchVerwaltungImpl();
+        bw.setNumVer(nummernverwaltung);
+        bw.setWebVer(webseiten);
+        
+        buchverwaltungservice = context.registerService(BuchVerwaltungImpl.class.getName(), bw, properties);
+        
     }
 
     public void stop(BundleContext context) throws Exception {
--- src/main/java/net/kalytta/bundlea1/buchver/BuchVerwaltungImpl.java
+++ src/main/java/net/kalytta/bundlea1/buchver/BuchVerwaltungImpl.java
@@ -7,6 +7,8 @@ package net.kalytta.bundlea1.buchver;
 
 import de.thkoeln.eks.osgi.basic.Buch;
 import de.thkoeln.eks.osgi.buchverw.BuchVerwaltung;
+import de.thkoeln.eks.osgi.zentralverwaltung.NummernVerwaltung;
+import de.thkoeln.eks.osgi.zentralverwaltung.WebSeiten;
 import java.util.ArrayList;
 
 /**
@@ -16,11 +18,16 @@ import java.util.ArrayList;
 public class BuchVerwaltungImpl implements BuchVerwaltung {
     
     private static final ArrayList<Buch> buecher = new ArrayList<Buch>();
+    private NummernVerwaltung numver;
+    private WebSeiten webver;
 
     public int neuesBuch(String string, int i) {
         
         Buch neu = new Buch();
-        int id = getNeueBuchRegNr();
+        
+        int id = numver.getNeueBuchRegNr();
+        
+        
         neu.setTitel(string);
         neu.setPreis(i);
         neu.setRegistrierungsnummer(id);
@@ -50,9 +57,17 @@ public class BuchVerwaltungImpl implements BuchVerwaltung {
         for(Buch b: buecher)
         {
             if(b.getRegistrierungsnummer()==i)
-                aufWebseiteStellen(b);
+                webver.aufWebseiteStellen(b);
         }
         
+    }
+
+    public void setWebVer(WebSeiten webseiten) {
+        this.webver = webseiten;
+    }
+
+    public void setNumVer(NummernVerwaltung nummernverwaltung) {
+        this.numver = nummernverwaltung;
     }
     
 }
