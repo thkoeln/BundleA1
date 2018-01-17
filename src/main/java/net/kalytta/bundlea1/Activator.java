732c79650398ca7ac345958cbceb9d1e1395d970
Endllesscrafter <endlesscrafter@gmail.com>
2018-01-17 19:53:51 +0100

    Added First Service Registration

--- src/main/java/net/kalytta/bundlea1/Activator.java
+++ src/main/java/net/kalytta/bundlea1/Activator.java
@@ -1,13 +1,20 @@
 package net.kalytta.bundlea1;
 
+import java.util.Hashtable;
+import net.kalytta.bundlea1.buchver.BuchVerwaltungImpl;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
-
+    
+    private ServiceRegistration<?> buchverwaltungservice;
+    
     public void start(BundleContext context) throws Exception {
         // TODO add activation code here
+        Hashtable<String,String> properties = new Hashtable<String, String>();
         
+        buchverwaltungservice = context.registerService(BuchVerwaltungImpl.class.getName(), new BuchVerwaltungImpl(), properties);
     }
 
     public void stop(BundleContext context) throws Exception {
